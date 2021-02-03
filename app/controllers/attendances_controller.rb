1c9248f19a4e31b00b5ff2a2d0700e57cb8701ef
alvaro <aaruizf@unal.edu.co>
2021-02-02 21:05:08 -0500

    Modify attendances controller

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,2 +1,28 @@
 class AttendancesController < ApplicationController
+  def index
+    @attendances = Attendance.all
+  end
+  def show
+    @attendance = Attendance.create(attendee_id: "1",attended_event_id: "1")
+    @event = Event.find(@attendance.attended_event_id)
+  end
+  def new
+    @attendance = Attendance.new
+  end
+  def create
+    @attendance = Attendance.new(attendance_params)
+    respond_to do |format|
+      if @attendance.save
+        format.html { redirect_to @attendance, notice: 'The attendance was successfully created.' }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+private
+
+  def attendance_params
+    params.require(:attendance).permit(:attendee_id,:attended_event_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   root to: 'users#new'
   resources :users
   resources :events
+  resources :attendances
 end
