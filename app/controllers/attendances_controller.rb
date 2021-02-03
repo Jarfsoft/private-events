4aa0e9388ee1290bee2fedb1a4b7eb7263124102
alvaro <aaruizf@unal.edu.co>
2021-02-03 17:20:42 -0500

    Modify attendances and events controllers

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,24 +1,27 @@
 class AttendancesController < ApplicationController
+  before_action :set_attendance, only: %i[show edit update destroy]
+
   def index
     @attendances = Attendance.all
   end
   def show
     #@attendance = Attendance.where("attended_event_id = ? ", )
-    @attendance = Attendance.find(params[:id])
+    #@attendance = Attendance.find(params[:id])
     @event = Event.find(@attendance.attended_event_id)
   end
   def new
     @attendance = Attendance.new
     @attendance.attendee_id = current_user.id
   end
+  def edit; end
   def create
-
-    #@attendance = Attendance.new(attendance_params)
-    @attendance = current_user.attendanced.build(attended_event_id: params[:event_id])
+    @event = Event.find(params[:attended_event_id])
+    @attendance = Attendance.new(attendance_params)
+    #@attendance = current_user.attendance.build(attended_event_id: params[:event_id])
     #@event = Event.find(params[:id])
     @attendance.attendee_id = current_user.id
-    @event = Event.find(@attendance.attended_event_id)
-    @attendance.attended_event_id = @event.id
+    #@event = Event.find(@attendance.attended_event_id)
+    #@attendance.attended_event_id = @event.id
     respond_to do |format|
       if @attendance.save
         format.html { redirect_to @attendance, notice: 'The attendance was successfully created.' }
@@ -27,10 +30,29 @@ class AttendancesController < ApplicationController
       end
     end
   end
+  def update
+    respond_to do |format|
+      if @attendance.update(attendance_params)
+        format.html { redirect_to @attendance, notice: 'Attendance was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
 
+  def destroy
+    @attendance.destroy
+    respond_to do |format|
+      format.html { redirect_to attendances_url, notice: 'Attendance was successfully destroyed.' }
+    end
+  end
 private
 
   def attendance_params
     params.require(:attendance).permit(:attendee_id,:attended_event_id)
+  end
+
+  def set_attendance
+    @attendance = Attendance.find(params[:id])
   end
 end
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,14 @@
 class EventsController < ApplicationController
+  before_action :set_event, only: %i[show edit update destroy]
   before_action :authenticate_user!, only: %i[new create]
 
   # GET /posts or /posts.json
   def index
     @events = Event.all
+    @event = Event.new
     @upcoming_events = @events.upcoming_events
     @past_events = @events.past_events
+    @attendance = Attendance.new
   end
 
   # GET /posts/1 or /posts/1.json
@@ -20,15 +23,17 @@ class EventsController < ApplicationController
   end
 
   # GET /posts/1/edit
-  def edit; end
+  def edit
+    @event = Event.find(params[:id])
+  end
 
   def create
-    @event = current_user.events.create(event_params)
+    @event = current_user.events.build(event_params)
     respond_to do |format|
       if @event.save
         format.html { redirect_to @event, notice: 'The event was successfully created.' }
       else
-        format.html { render :new, status: :unprocessable_entity }
+        format.html { render :new, alert: 'Please Sign In.' }
       end
     end
   end
@@ -38,4 +43,9 @@ class EventsController < ApplicationController
   def event_params
     params.require(:event).permit(:title, :date, :location)
   end
+
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
 end
