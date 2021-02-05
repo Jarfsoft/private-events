9feab0bfaa1ae9ae67139e3ef5fdd7217edeb8c0
Jarfsoft <andresjex@hotmail.com>
2021-02-05 10:30:29 -0600

    fix linters

--- db/migrate/20210202041208_create_events.rb
+++ db/migrate/20210202041208_create_events.rb
@@ -8,7 +8,6 @@ class CreateEvents < ActiveRecord::Migration[6.1]
       t.timestamps
     end
     add_reference :events, :creator, null: false, index: {unique: true}, foreign_key: true
-    #add_column :events, :creator_id, :integer, index: true, foreign_key:true
 
   end
 end
